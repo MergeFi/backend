215153bec19b6156ee2a18c0ff2b1ea8563fdb00
MyDude92 <88647056+MyDude92@users.noreply.github.com>
2026-09-20 21:48:46 +1000

    feat(validators): add IsStellarAddress decorator using StrKey validation (#60)

--- src/common/validators/stellar-address.validator.ts
+++ src/common/validators/stellar-address.validator.ts
@@ -1,1 +1,27 @@
-export const test = 1;
+import { registerDecorator, ValidationOptions } from 'class-validator';
+import { StrKey } from '@stellar/stellar-sdk';
+
+/**
+ * Validates that the decorated property is a valid Stellar Ed25519 public key (StrKey G...).
+ */
+export function IsStellarAddress(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'isStellarAddress',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: any) {
+          if (typeof value !== 'string') {
+            return false;
+          }
+          return StrKey.isValidEd25519PublicKey(value);
+        },
+        defaultMessage() {
+          return '$property must be a valid Stellar public key (StrKey G...)';
+        },
+      },
+    });
+  };
+}
